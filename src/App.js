0bcb50577474251d1199ae6ea8f0d5cefaa78b37
ilegwmam <ilegwmam@gmail.com>
2021-05-23 12:24:25 +0900

    Init default settings, App, Header, AboveGame, Game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import AboveGame from './component/AboveGame';
+import Game from './component/Game';
+import Header from './component/Header';
 
 export default function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+      <Header />
+      <AboveGame />
+      <Game />
     </div>
   );
 }
